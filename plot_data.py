477901546304c4400b84d5cc4a44979ba5c24380
Eva Elbing <eva.elbing@cri-paris.org>
2022-04-01 15:26:31 +0200

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.title('COVID Dashboard')
+st.header('Global Covid-19 cases')
+st.markdown('This Graph displays the worldwide cases of Covid-19 over time')
+
 df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 
 dataframe = df["total_cases"]
