bd30019e046a6871011ef393998048d3857b0794
Dennis Núñez-Fernández <dennishnf@gmail.com>
2022-04-08 12:19:34 +0200

    add country selection and data

--- plot_data.py
+++ plot_data.py
@@ -45,13 +45,44 @@ location_selector = sidebar.selectbox(
     "Select a Location",
     locations
 )
-st.markdown(f"## Currently Selected: {location_selector}")
+st.markdown(f"# Currently Selected {location_selector}")
+
+show_data = sidebar.checkbox("Show Data")
+
+trend_data = data.query(f"location=='{location_selector}'").\
+    groupby(pd.Grouper(key="date", 
+    freq="1D")).aggregate(new_cases=("new_cases", "sum"),
+    new_deaths = ("new_deaths", "sum"),
+    new_vaccinations = ("new_vaccinations", "sum"),
+    new_tests = ("new_tests", "sum")).reset_index()
+
+trend_data["date"] = trend_data.date.dt.date
+
+new_cases = sidebar.checkbox("New Cases")
+new_deaths = sidebar.checkbox("New Deaths")
+new_vaccinations = sidebar.checkbox("New Vaccinations")
+new_tests = sidebar.checkbox("New Tests")
+
+lines = [new_cases, new_deaths, new_vaccinations, new_tests]
+line_cols = ["new_cases", "new_deaths", "new_vaccinations", "new_tests"]
+trends = [c[1] for c in zip(lines,line_cols) if c[0]==True]
+
+if show_data:
+    tcols = ["date"] + trends
+    st.dataframe(trend_data[tcols])
 
 daily_cases = data.groupby(pd.Grouper(key="date", freq="1D")).aggregate(new_cases=("new_cases", "sum")).reset_index()
 fig = daily_cases.iplot(kind="line", asFigure=True, 
                         x="date", y="new_cases")
 st.plotly_chart(fig)
 
+
+
+subplots=sidebar.checkbox("Show Subplots", True)
+if len(trends)>0:
+    fig=trend_data.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
+                         x="date", y=trends, title=f"{trend_level} Trend of {', '.join(trends)}.", subplots=subplots)
+    st.plotly_chart(fig, use_container_width=False)
 
 
 
