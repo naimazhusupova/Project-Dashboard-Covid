ca670917ccaf5b770bb2b6bdba21b5a3f063a122
Eva Elbing <eva.elbing@cri-paris.org>
2022-04-08 11:49:52 +0200

    Update plot_data.py

--- plot_data.py
+++ plot_data.py
@@ -4,38 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 st.title('COVID Dashboard')
-st.header('Global Covid-19 cases')
+st.markdown('This Dashboard displays the worldwide cases of Covid-19 over time. It is based on the data by Our World in Data. We also have a tutorial on how to build this Dashboard by yourself.')
 st.sidebar.title("About this dashboard Project")
-st.markdown('This Graph displays the worldwide cases of Covid-19 over time')
 st.sidebar.markdown("Here we will give further information about our project or implement some cool widgets!")
 
 df = pd.read_csv("https://covid.ourworldindata.org/data/owid-covid-data.csv")
 
 dataframe = df["total_cases"]
-
-
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-import cufflinks
-
-@st.cache
-def get_data(url):
-    df = pd.read_csv(url)
-    df["date"] = pd.to_datetime(df.date).dt.date
-    df['date'] = pd.DatetimeIndex(df.date)
-
-    return df
-
-url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
-data = get_data(url)
-
-daily_cases = data.groupby(pd.Grouper(key="date", freq="1D")).aggregate(new_cases=("new_cases", "sum")).reset_index()
-fig = daily_cases.iplot(kind="line", asFigure=True, 
-                        x="date", y="new_cases")
-#st.plotly_chart(fig)
-
 
 
 
@@ -70,7 +45,7 @@ location_selector = sidebar.selectbox(
     "Select a Location",
     locations
 )
-st.markdown(f"# Currently Selected {location_selector}")
+st.markdown(f"## Currently Selected: {location_selector}")
 
 daily_cases = data.groupby(pd.Grouper(key="date", freq="1D")).aggregate(new_cases=("new_cases", "sum")).reset_index()
 fig = daily_cases.iplot(kind="line", asFigure=True, 
