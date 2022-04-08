265d8d1b72e8f13072bbc27fe8e6599dfd304301
naimazhusupova <naima1zhusupova@gmail.com>
2022-04-08 16:21:58 +0200

    test

--- plot_data.py
+++ plot_data.py
@@ -25,49 +25,47 @@ locations = data.location.unique().tolist()
 
 sidebar = st.sidebar
 
-analysis_type = sidebar.radio("Analysis Type", ["Multiple"])
 
-if analysis_type=="Multiple":
-    selected = sidebar.multiselect("Select Locations ", locations)
-    st.markdown(f"### Selected Locations: {', '.join(selected)}")
+selected = sidebar.multiselect("Select Locations ", locations)
+st.markdown(f"### Selected Locations: {', '.join(selected)}")
 
-    trend_data = data.query(f"location in {selected}").\
-        groupby(["location", pd.Grouper(key="date", 
-        freq="1D")]).aggregate(new_cases=("new_cases", "sum"),
-        new_deaths = ("new_deaths", "sum"),
-        new_vaccinations = ("new_vaccinations", "sum"),
-        new_tests = ("new_tests", "sum")).reset_index()
+trend_data = data.query(f"location in {selected}").\
+    groupby(["location", pd.Grouper(key="date", 
+    freq="1D")]).aggregate(new_cases=("new_cases", "sum"),
+    new_deaths = ("new_deaths", "sum"),
+    new_vaccinations = ("new_vaccinations", "sum"),
+    new_tests = ("new_tests", "sum")).reset_index()
 
-    trend_data["date"] = trend_data.date.dt.date
+trend_data["date"] = trend_data.date.dt.date
 
-    new_cases = sidebar.checkbox("New Cases")
-    new_deaths = sidebar.checkbox("New Deaths")
-    new_vaccinations = sidebar.checkbox("New Vaccinations")
-    new_tests = sidebar.checkbox("New Tests")
+new_cases = sidebar.checkbox("New Cases")
+new_deaths = sidebar.checkbox("New Deaths")
+new_vaccinations = sidebar.checkbox("New Vaccinations")
+new_tests = sidebar.checkbox("New Tests")
 
-    lines = [new_cases, new_deaths, new_vaccinations, new_tests]
-    line_cols = ["new_cases", "new_deaths", "new_vaccinations", "new_tests"]
-    trends = [c[1] for c in zip(lines,line_cols) if c[0]==True]
+lines = [new_cases, new_deaths, new_vaccinations, new_tests]
+line_cols = ["new_cases", "new_deaths", "new_vaccinations", "new_tests"]
+trends = [c[1] for c in zip(lines,line_cols) if c[0]==True]
 
-    ndf = pd.DataFrame(data=trend_data.date.unique(),columns=["date"])
+ndf = pd.DataFrame(data=trend_data.date.unique(),columns=["date"])
 
-    for s in selected:
-        new_cols = ["date"]+[f"{s}_{c}" for c in line_cols]
-        tdf = trend_data.query(f"location=='{s}'")
-        tdf.drop("location", axis=1, inplace=True)
-        tdf.columns=new_cols
-        ndf=ndf.merge(tdf,on="date",how="inner")
+for s in selected:
+    new_cols = ["date"]+[f"{s}_{c}" for c in line_cols]
+    tdf = trend_data.query(f"location=='{s}'")
+    tdf.drop("location", axis=1, inplace=True)
+    tdf.columns=new_cols
+    ndf=ndf.merge(tdf,on="date",how="inner")
 
-    new_trends = []
-    for c in trends:
-        new_trends.extend([f"{s}_{c}" for s in selected])
+new_trends = []
+for c in trends:
+    new_trends.extend([f"{s}_{c}" for s in selected])
 
-    if len(trends)>0:
-        st.markdown("### Trend of Selected Locations")
+if len(trends)>0:
+    st.markdown("### Trend of Selected Locations")
 
-        fig=ndf.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
-                            x="date", y=new_trends, title=f"Trend of {', '.join(trends)}.", subplots=False)
-        st.plotly_chart(fig, use_container_width=False)
+    fig=ndf.iplot(kind="line", asFigure=True, xTitle="Date", yTitle="Values",
+                        x="date", y=new_trends, title=f"Trend of {', '.join(trends)}.", subplots=False)
+    st.plotly_chart(fig, use_container_width=False)
 
 
 
