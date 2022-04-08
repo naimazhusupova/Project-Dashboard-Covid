e7d67424569b60d12d7146640f0b5fba9d00cd0d
Dennis Núñez-Fernández <dennishnf@gmail.com>
2022-04-08 11:21:05 +0200

    add country selection

--- plot_data.py
+++ plot_data.py
@@ -26,3 +26,78 @@ st.write('This Graph shows Covid-19 total cases dynamics per continent')
 df2 = df['continent'].value_counts().head(10).sort_values(ascending=False)
 st.bar_chart(df2)
 
+
+
+
+
+
+
+
+
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import cufflinks
+
+@st.cache
+def get_data(url):
+    df = pd.read_csv(url)
+    df["date"] = pd.to_datetime(df.date).dt.date
+    df['date'] = pd.DatetimeIndex(df.date)
+
+    return df
+
+url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+data = get_data(url)
+
+daily_cases = data.groupby(pd.Grouper(key="date", freq="1D")).aggregate(new_cases=("new_cases", "sum")).reset_index()
+fig = daily_cases.iplot(kind="line", asFigure=True, 
+                        x="date", y="new_cases")
+st.plotly_chart(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+import cufflinks as cf
+cf.go_offline()
+cf.set_config_file(offline=False, world_readable=True)
+
+
+def get_data(url):
+    df = pd.read_csv(url)
+    df["date"] = pd.to_datetime(df.date).dt.date
+    df['date'] = pd.DatetimeIndex(df.date)
+
+    return df
+
+
+url = "https://covid.ourworldindata.org/data/owid-covid-data.csv"
+data = get_data(url)
+
+locations = data.location.unique().tolist()
+
+sidebar = st.sidebar
+location_selector = sidebar.selectbox(
+    "Select a Location",
+    locations
+)
+st.markdown(f"# Currently Selected {location_selector}")
+
+daily_cases = data.groupby(pd.Grouper(key="date", freq="1D")).aggregate(new_cases=("new_cases", "sum")).reset_index()
+fig = daily_cases.iplot(kind="line", asFigure=True, 
+                        x="date", y="new_cases")
+st.plotly_chart(fig)
+
+
+
+
