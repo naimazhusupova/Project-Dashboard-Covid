345285ed071143d8f8fee0295ed61720a02cd39d
naimazhusupova <naima1zhusupova@gmail.com>
2022-04-22 11:54:16 +0200

    edit

--- plot_data.py
+++ plot_data.py
@@ -34,13 +34,16 @@ sidebar = st.sidebar
 
 
 #Date selection
+
 st.sidebar.markdown("Choose a date")
-start_date = st.sidebar.date_input('Start date')
-end_date = st.sidebar.date_input('End date')
+start_date = st.sidebar.date_input('Start date', datetime.date(2020, 2, 24))
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+end_date = st.sidebar.date_input('End date', tomorrow)
 if start_date < end_date:
     st.sidebar.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
 else:
     st.sidebar.error('Error: End date must fall after start date.')
+
 
 
 start_date = np.datetime64(start_date)
